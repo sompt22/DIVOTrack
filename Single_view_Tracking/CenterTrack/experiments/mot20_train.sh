3c6da113495e3eb0b29397c2d978f377c6bbec34
sompt22 <femres87@gmail.com>
2023-10-19 20:42:05 +0300

    CenterTrack mot20 tranining

--- Single_view_Tracking/CenterTrack/experiments/mot20_train.sh
+++ Single_view_Tracking/CenterTrack/experiments/mot20_train.sh
@@ -1,3 +1,3 @@
 cd src
-python main.py tracking --exp_id mot20 --dataset mot20 --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --gpus 0 --load_model ../models/crowdhuman.pth --num_epochs 30 --batch_size 8
+python main.py tracking --exp_id mot20 --dataset mot20 --pre_hm --ltrb_amodal --same_aug --hm_disturb 0.05 --lost_disturb 0.4 --fp_disturb 0.1 --gpus 0 --load_model ../models/crowdhuman.pth --num_epochs 30 --batch_size 16
 cd ..
