1370bf5657628682651b733f8444209ba199bafb
sompt22 <femres87@gmail.com>
2024-01-08 23:23:15 +0300

    Inference script added to CenterTrack

---
+++ Single_view_Tracking/CenterTrack/experiments/inference.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script runs inference on a video file
+experiment_name=$1
+model_path=$(realpath $2)
+video_path=$(realpath $3)
+
+
+cd src
+# infer --motchallenge saves inference results
+python demo.py tracking,embedding --exp_id $experiment_name --max_age 15 --debug 1 --load_model $model_path --num_classes 1 --demo $video_path 
+cd ..
